c40955e3c4a7d98552caed6766dcc96d5af7111d
Adam Webb <adamwebb@microsoft.com>
2025-07-17 00:24:21 -0700

    feat: add CDN Front Door resources and diagnostic settings for API Management

--- infra/apim.tf
+++ infra/apim.tf
@@ -1,19 +1,52 @@
 resource "azurerm_api_management" "apim" {
-  name                = "${local.abbrs.apiManagementService}${random_id.random_deployment_suffix.hex}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  name                 = "${local.abbrs.apiManagementService}${random_id.random_deployment_suffix.hex}"
+  location             = data.azurerm_resource_group.rg.location
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_type = var.apim_virtual_network_type
   virtual_network_configuration {
     subnet_id = azapi_resource.apim_subnet.id
   }
-  publisher_email     = var.publisher_email
-  publisher_name      = var.publisher_name
-  sku_name            = "${var.sku}_${var.sku_count}"
+  publisher_email = var.publisher_email
+  publisher_name  = var.publisher_name
+  sku_name        = "${var.sku}_${var.sku_count}"
   identity {
     type = "SystemAssigned"
   }
   lifecycle {
     ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "apim_diagnostics" {
+  name                       = "${local.abbrs.apiManagementService}${random_id.random_deployment_suffix.hex}_diagnostics"
+  target_resource_id         = azurerm_api_management.apim.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+
+  enabled_log {
+    category = "GatewayLogs"
+  }
+  enabled_log {
+    category = "WebSocketConnectionLogs"
+  }
+  enabled_log {
+    category = "DeveloperPortalAuditLogs"
+  }
+  enabled_log {
+    category = "GatewayLlmLogs"
+  }
+  enabled_metric {
+    category = "AllMetrics"
+  }
+}
+
+resource "azurerm_api_management_logger" "apim_logger" {
+  name                = "${local.abbrs.apiManagementService}${random_id.random_deployment_suffix.hex}_logger"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_id         = azurerm_application_insights.app_insights.id
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.app_insights.instrumentation_key
   }
 }
 
@@ -26,8 +59,8 @@ resource "azurerm_api_management_api" "nr-ai-form" {
   path                = "nr-ai-form"
   protocols           = ["https"]
   service_url         = "https://${azurerm_linux_web_app.app_service.default_hostname}"
-   import {
+  import {
     content_format = "openapi"
     content_value  = file("./../src/docs/openapi.yaml")
-  } 
+  }
 }
