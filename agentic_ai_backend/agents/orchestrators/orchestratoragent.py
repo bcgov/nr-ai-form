4f6950d84f71a4499a08372fb0f21a9ff66d54cd
Abin Antony <abin.antony@aot-technologies.com>
2026-03-02 11:54:59 -0800

    dev-SHOWCASE-4188 Redis tryout code Temp!

--- agentic_ai_backend/agents/orchestrators/orchestratoragent.py
+++ agentic_ai_backend/agents/orchestrators/orchestratoragent.py
@@ -125,13 +125,13 @@ async def orchestrate_a2a(query: str,
             await agent._notify_thread_of_new_messages(thread, input_messages, response_messages)
 
         # Save Thread State
-        if thread:
-            try:
-                print(f"Saving thread {thread_id} to Redis...")          
-                await db_utils.save_thread_state(thread_id, thread)
-                print("Thread state saved.")
-            except Exception as e:
-                print(f"Error saving thread state: {e}")
+        # if thread:
+        #     try:
+        #         print(f"Saving thread {thread_id} to Redis...")          
+        #         await db_utils.save_thread_state(thread_id, thread)
+        #         print("Thread state saved.")
+        #     except Exception as e:
+        #         print(f"Error saving thread state: {e}")
        
         # Process and display results
         if final_data and isinstance(final_data, list):
