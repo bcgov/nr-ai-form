f4dc9bc40c2b50fd7f4c6b86b38f819540e7ce1a
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2026-04-10 13:10:21 -0700

    Remove caching locally

--- agentic_ai_backend/agents/orchestrators/services/suggested_questions_service.py
+++ agentic_ai_backend/agents/orchestrators/services/suggested_questions_service.py
@@ -12,14 +12,15 @@ class SuggestedQuestionsService:
 
     def fetch_suggested_questions(self) -> Optional[str]:
 
-        if 'suggested_questions' in self.cache:            
-            return self.cache['suggested-questions']
+        # TODO: Cache the response
+        # if 'suggested-questions' in self.cache:            
+        #     return self.cache['suggested-questions']
 
         try:
             blob_name = f"{self.directory_path}/questions.json"
             
             suggested_questions = self.blob_service.read_blob_text(self.container_name, blob_name)
-            self.cache['suggested-questions'] = suggested_questions
+            # self.cache['suggested-questions'] = suggested_questions
             return suggested_questions
         except Exception as e:
             print(f"Error fetching suggested questions: {e}")
