f30eb5cb9c222d8a1e7bfecfda14a4145cdc5502
Ann Mary Jacob <77353155+ann-aot@users.noreply.github.com>
2026-07-29 19:42:07 -0400

    Update client.js

--- client-scripts/client.js
+++ client-scripts/client.js
@@ -472,7 +472,9 @@ else {
                 composeemailforsignaturerequest: FormSteps.STEP9_CO_APPLICANT_COMPOSE_EMAIL,
                 complete: FormSteps.STEP10_COMPLETE,
                 pubsubmitteraddress: FormSteps.SHARED_ADDRESS,
-                step9signatures: FormSteps.STEP9_CO_APPLICANT_SIGNATURES
+                step9signatures: FormSteps.STEP9_CO_APPLICANT_SIGNATURES,   
+                editindividual: FormSteps.STEP7_CO_APPLICANT_ADD_AN_INDIVIDUAL,
+                editorganization: FormSteps.STEP7_CO_APPLICANT_ADD_A_BUSINESS_APPLICANT
             };
             return paneHeaderStepMap[paneHeaderText] || null;
         }
